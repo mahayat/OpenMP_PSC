b863fa0756b60dee1311aaf01a7266815632e7be
Md Abul Hayat <mahayat@br006.pvt.bridges.psc.edu>
2020-11-03 18:14:53 -0500

    add after completing2

--- Exercises/OpenMP/Prime/prime_serial.c
+++ Exercises/OpenMP/Prime/prime_serial.c
@@ -1,12 +1,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 
+%% #pragma omp parallel 
 int main ( int argc, char *argv[] ){
-
-  int n = 500000;
+  int n = 50000;
   int not_primes=0;
   int i,j;
 
+%% #pragma omp for reduction(+:not_primes)
   for ( i = 2; i <= n; i++ ){
     for ( j = 2; j < i; j++ ){
       if ( i % j == 0 ){
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# OpenMP_PSC
-XSEDE HPC Workshop: OpenMP
+## XSEDE HPC Workshop (Nov 3, 2020)
+
+- [Topics covered](https://www.psc.edu/hpc-workshop-series/openmp)
+- Videos were recorded 
+
+```
+module load pgi
+pgcc -o code -mp code.c
+export OMP_NUM_THREADS=8
+./code
+```
